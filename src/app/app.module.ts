f4261c20b6285338731a49270743be55be73b8d6
Evan Haggard <61989628+flyaway12345@users.noreply.github.com>
2022-08-31 21:18:46 -0400

    added youtube service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InfocardComponent } from './infocard/infocard.component';
@@ -12,6 +12,7 @@ import { InfocardComponent } from './infocard/infocard.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
