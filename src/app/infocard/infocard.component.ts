e7df61da8628b192c84d44e24adab8e9e0341fdf
Evan Haggard <61989628+flyaway12345@users.noreply.github.com>
2022-08-31 21:11:55 -0400

    added card dummy

---
+++ src/app/infocard/infocard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-infocard',
+  templateUrl: './infocard.component.html',
+  styleUrls: ['./infocard.component.css']
+})
+export class InfocardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
