b0491648c126bc3cb7765add08a7f9c4625138c6
Evan Haggard <61989628+flyaway12345@users.noreply.github.com>
2022-09-02 18:04:57 -0400

    Thank Christ this is done

--- src/app/audiodex/audiodex.component.ts
+++ src/app/audiodex/audiodex.component.ts
@@ -9,10 +9,8 @@ import { TestService } from '../services/test.service';
   styleUrls: ['./audiodex.component.css']
 })
 export class AudiodexComponent implements OnInit {  
-  //ik this is not the best way to do this, but i want it to work ill refine later
   pokedexLength = 5;
   pokemonDataArray: any[] = Array.apply(null, Array(this.pokedexLength)).map(function (x, i) { return i; })
- // pokemonDataArray: any[] = [1, 2, 3, 4, 5];
   pokemonEndpoint:string = '';
   
 
@@ -30,17 +28,6 @@ export class AudiodexComponent implements OnInit {
     }
     console.log(this.pokemonDataArray);
   }
-  // getPokemonSpeciesData(){
-  //   for(let i = 0; i < this.pokemonDataArray.length; i++){
-  //     this.pokemonEndpoint = this.pokeAPI.getPokemonFromPokeAPIURL + (i+1);
-  //     console.log(this.pokemonEndpoint);
-  //     this.pokeAPI.getData(this.pokemonEndpoint).subscribe(data=>this.pokemonDataArray[i]=data);
-  //     console.log('this.getData fired');
-  //     console.log(this.pokemonDataArray[i]);
-      
-  //   }
-  //   console.log(this.pokemonDataArray);
-  // }
 
   ngOnInit(): void {
     this.getPokemonData();
