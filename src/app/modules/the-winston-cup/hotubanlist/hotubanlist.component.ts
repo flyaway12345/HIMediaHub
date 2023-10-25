5a98c4f82eb076fde41b72d99a7c874352d1f1b5
Evan Haggard <61989628+flyaway12345@users.noreply.github.com>
2023-10-25 18:02:17 -0400

    banlist updated

--- src/app/modules/the-winston-cup/hotubanlist/hotubanlist.component.ts
+++ src/app/modules/the-winston-cup/hotubanlist/hotubanlist.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HotubanlistComponent implements OnInit {
 
-  constructor() { }
+  banlist: any;
+  url: string = '';
+  public constructor(private http: HttpClient) {}
+
+
 
   ngOnInit(): void {
+    this.http.get(this.url).subscribe(res => {
+      this.banlist = res;
+  });
+  
   }
-
 }
