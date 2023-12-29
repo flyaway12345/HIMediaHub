1be52a91324a0c2b91dbcd20eba49555a4b358e1
Evan Haggard <61989628+flyaway12345@users.noreply.github.com>
2023-12-29 16:13:06 -0500

    blog brogress 2

--- src/app/modules/blog-home/blog/blog.component.ts
+++ src/app/modules/blog-home/blog/blog.component.ts
@@ -1,16 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css'
 })
 export class BlogComponent implements OnInit {
   blogData: any;
   url: string = 'https://raw.githubusercontent.com/flyaway12345/HIMediaHub/main/src/app/modules/blog-home/postlist.json';
+openArticle: any;
   public constructor(private http: HttpClient) {}
 
 
--- src/app/modules/blog-home/posts/posts.component.ts
+++ src/app/modules/blog-home/posts/posts.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
  
 import { ActivatedRoute } from '@angular/router';
  
@@ -9,13 +10,15 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.css'
 })
-export class PostsComponent {
-  post: string = "";
-  href: string = ""; 
-  constructor(private route: ActivatedRoute) { } 
+export class PostsComponent implements OnInit {
+  blogData: any;
+  url: string = 'https://raw.githubusercontent.com/flyaway12345/HIMediaHub/main/src/app/modules/blog-home/postlist.json';
+  public constructor(private http: HttpClient) {}
+  openArticle: string ="helloworld";
   ngOnInit(): void {
-      let articleName = this.route.snapshot.paramMap.get('article'); 
-      this.href = window.location.href; 
-      this.post = './assets/posts/' + articleName + '.md'; 
-  }
+    this.http.get(this.url).subscribe(res => {
+      this.blogData = res;
+  });
+  
+}
 }
