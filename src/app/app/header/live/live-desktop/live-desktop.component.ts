c3d522f0411db40ab10f7fa4ab1b28a279d89b4d
flyaway12345 <evanhaggard123@gmail.com>
2024-06-02 23:09:38 -0400

    added swap between twitch and kick

--- src/app/app/header/live/live-desktop/live-desktop.component.ts
+++ src/app/app/header/live/live-desktop/live-desktop.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { TwitchComponent } from './twitch/twitch.component';
+import { KickComponent } from "./kick/kick.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-live-desktop',
-  standalone: true,
-  imports: [TwitchComponent],
-  templateUrl: './live-desktop.component.html',
-  styleUrl: './live-desktop.component.css'
+    selector: 'app-live-desktop',
+    standalone: true,
+    templateUrl: './live-desktop.component.html',
+    styleUrl: './live-desktop.component.css',
+    imports: [TwitchComponent, KickComponent,CommonModule]
 })
 export class LiveDesktopComponent {
-
+  twitchOrKick:boolean = false;
+  playerSwitch() {
+    this.twitchOrKick = !this.twitchOrKick;
+  }
 }
--- src/app/app/header/live/live-mobile/live-mobile.component.ts
+++ src/app/app/header/live/live-mobile/live-mobile.component.ts
@@ -1,28 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { TwitchComponent } from './twitch/twitch.component';
+import { KickComponent } from "./kick/kick.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-live-mobile',
-  standalone: true,
-  imports:[
-    RouterOutlet,
-    RouterLink,
-    RouterLinkActive,
-    TwitchComponent
-  ],
-  templateUrl: './live-mobile.component.html',
-  styleUrls: ['./live-mobile.component.css']
+    selector: 'app-live-mobile',
+    standalone: true,
+    templateUrl: './live-mobile.component.html',
+    styleUrls: ['./live-mobile.component.css'],
+    imports: [
+        RouterOutlet,
+        RouterLink,
+        RouterLinkActive,
+        TwitchComponent,
+        KickComponent,
+        CommonModule
+    ]
 })
 export class LiveMobileComponent implements OnInit {
+
   mobHeight: any;
   mobWidth: any;
   liveswitch:boolean = false;
+  
   constructor() { 
     this.mobHeight = (window.screen.height) + "px";
     this.mobWidth = (window.screen.width) + "px";
       console.log(this.mobHeight);
       console.log(this.mobWidth)
+  }
+  twitchOrKick:boolean = false;
+  playerSwitch() {
+    this.twitchOrKick = !this.twitchOrKick;
   }
 
   ngOnInit(): void {
