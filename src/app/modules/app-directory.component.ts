218a296ae78ecd9d61ed81881f1e2ca70142195f
Evan Haggard <61989628+flyaway12345@users.noreply.github.com>
2024-06-18 16:43:06 -0400

    updated appdir

--- src/app/modules/app-directory.component.ts
+++ src/app/modules/app-directory.component.ts
@@ -20,12 +20,17 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 export class AppDirectoryComponent implements OnInit {
   textSearch: any;
   appList: any;
+  displayApp: boolean = true;
   url: string = 'https://raw.githubusercontent.com/flyaway12345/HIMediaHub/main/src/app/modules/modulesList.json';
   public constructor(private http: HttpClient) {}
   
   public orderByID(): void {
     this.appList = this.appList.sort((a: { id: number; }, b: { id: number; }) => b.id - a.id);
     console.log("sorted");
+  }
+  public performFilter() {
+    this.appList = this.appList.filter((res: { display: boolean; }) => res.display == true);
+    console.log("filtered");
   }
   
   ngOnInit(): void {
